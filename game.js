d1b0b76bc98a4d6f64d80fa6e39ca534bec9a0bb
ValeGottardello <valengottardello@hotmail.com>
2023-02-24 12:58:11 -0800

    last update, css mediaquery, js tie fixed, value reset

--- game.js
+++ game.js
@@ -31,17 +31,6 @@ let c1 = document.querySelector("#c-one")
 let c2 = document.querySelector("#c-two")
 let c3 = document.querySelector("#c-three")
 let counter = 0
-// winningOptions 
-// a1+a2+a3
-// b1+b2+b3
-// c1+c2+c3
-
-// a1+b1+c1
-// a2+b2+c2
-// a3+b3+c3
-
-// a1+b2+c3
-// a3+b2+c1
 
 //player one = x
 //player two = o
@@ -52,57 +41,57 @@ function gameTicTacToe(event) {
     //checks if any of the winning arrays contains "x" or "o"
     //X WINS
     if (a1.classList.contains("X") && a2.classList.contains("X") && a3.classList.contains("X") ||
-        b1.classList.contains("X") && b2.classList.contains("X") && b3.classList.contains("X") ||
-        c1.classList.contains("X") && c2.classList.contains("X") && c3.classList.contains("X") ||
-        a1.classList.contains("X") && b1.classList.contains("X") && c1.classList.contains("X") ||
-        a2.classList.contains("X") && b2.classList.contains("X") && c2.classList.contains("X") ||
-        a3.classList.contains("X") && b3.classList.contains("X") && c3.classList.contains("X") ||
-        a1.classList.contains("X") && b2.classList.contains("X") && c3.classList.contains("X") ||
-        a3.classList.contains("X") && b2.classList.contains("X") && c1.classList.contains("X")) {
+    b1.classList.contains("X") && b2.classList.contains("X") && b3.classList.contains("X") ||
+    c1.classList.contains("X") && c2.classList.contains("X") && c3.classList.contains("X") ||
+    a1.classList.contains("X") && b1.classList.contains("X") && c1.classList.contains("X") ||
+    a2.classList.contains("X") && b2.classList.contains("X") && c2.classList.contains("X") ||
+    a3.classList.contains("X") && b3.classList.contains("X") && c3.classList.contains("X") ||
+    a1.classList.contains("X") && b2.classList.contains("X") && c3.classList.contains("X") ||
+    a3.classList.contains("X") && b2.classList.contains("X") && c1.classList.contains("X")) {
         //update score of player
         player1score++
         numScore1.textContent = `${player1score}`
         //Change text with the winner
         whoWinsParr.textContent = `${name1} win!`
         whoWinsParr.style.display = "block"
+        whoWinsParr.classList.add("animate__zoomInDown")
         h2.style.display = "none"
         //disble the buttons when someone win
-        for(let i =0;i < boxes.length ; i++) {
-            boxes[i].disabled = true
-            
-        }
-
+        boxes.forEach(box => {
+            box.disabled = true
+        })
         //Display button reset
-        setTimeout(reStart, 2000)
+        setTimeout(reStart, 3000)
         //O WINS
     } else if (a1.classList.contains("O") && a2.classList.contains("O") && a3.classList.contains("O") ||
-        b1.classList.contains("O") && b2.classList.contains("O") && b3.classList.contains("O") ||
-        c1.classList.contains("O") && c2.classList.contains("O") && c3.classList.contains("O") ||
-        a1.classList.contains("O") && b1.classList.contains("O") && c1.classList.contains("O") ||
-        a2.classList.contains("O") && b2.classList.contains("O") && c2.classList.contains("O") ||
-        a3.classList.contains("O") && b3.classList.contains("O") && c3.classList.contains("O") ||
-        a1.classList.contains("O") && b2.classList.contains("O") && c3.classList.contains("O") ||
-        a3.classList.contains("O") && b2.classList.contains("O") && c1.classList.contains("O")) {
+    b1.classList.contains("O") && b2.classList.contains("O") && b3.classList.contains("O") ||
+    c1.classList.contains("O") && c2.classList.contains("O") && c3.classList.contains("O") ||
+    a1.classList.contains("O") && b1.classList.contains("O") && c1.classList.contains("O") ||
+    a2.classList.contains("O") && b2.classList.contains("O") && c2.classList.contains("O") ||
+    a3.classList.contains("O") && b3.classList.contains("O") && c3.classList.contains("O") ||
+    a1.classList.contains("O") && b2.classList.contains("O") && c3.classList.contains("O") ||
+    a3.classList.contains("O") && b2.classList.contains("O") && c1.classList.contains("O")) {
         //update score of player
         player2score++
         numScore2.textContent = `${player2score}`
         //Change text with the winner
         whoWinsParr.textContent = `${name2} win!`
         whoWinsParr.style.display = "block"
+        whoWinsParr.classList.add("animate__zoomInDown")
         h2.style.display = "none"
         //disabled the buttons when o win
-        boxes.forEach(box =>{
+        boxes.forEach(box => {
             box.disabled = true
-        }) 
+        })
         //Display button reset
-        setTimeout(reStart, 2000)
+        setTimeout(reStart, 3000)
     } else {
-        counter++ 
-        if (counter === 9){    
-            text.textContent = `It's a tie!`
+        counter++
+        if (counter === 9) {
+            h2.textContent = `It's a tie!`
             setTimeout(reStart, 3000)
         }
-    }     
+    }
 }
 //TURNS
 function switchPlayers(event) {
@@ -115,9 +104,12 @@ function switchPlayers(event) {
         //this change the player turn
         player = 2;
         // change text of turn of player
-        h2.textContent = `Turn is for: ${name2}`
+        h2.classList.remove("animate__tada")
+        h2.classList.add("animate__jello")
+        h2.textContent = `The turn is for: ${name2}`
         //change style of the box
         event.target.style.backgroundColor = "#FFBD00"
+        event.target.classList.add("animate__flip")
 
     } else {
         //player two play
@@ -126,10 +118,13 @@ function switchPlayers(event) {
         event.target.textContent = "O"
         //changes player turn
         player = 1;
-        // change text of turn of player
+        // change text of turn of player 
+        h2.classList.add("animate__tada")
+        h2.classList.remove("animate__jello")
         h2.textContent = `The turn is for: ${name1}`
         //change style of the box
         event.target.style.backgroundColor = "#f72584"
+        event.target.classList.add("animate__flip")
     }
     //call game function
     gameTicTacToe(event);
@@ -141,18 +136,23 @@ function reStart(event) {
     player = 1
     //display who win 
     whoWinsParr.style.display = "none"
+    whoWinsParr.classList.remove("animate__zoomInDown")
     h2.style.display = "block"
     h2.textContent = `The turn is for:${name1}`
+    h2.classList.remove("animate__tada")
     //reset the board
-    for (let i = 0; i < boxes.length ; i++) {
+    for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.backgroundColor = "#ffbb00cb"
         boxes[i].classList.remove("X")
         boxes[i].classList.remove("O")
         boxes[i].disabled = false
         boxes[i].textContent = ""
-    }  
+        boxes[i].classList.remove("animate__flip")
+    }
     //for ties 
     counter = 0
+    //animation
+    //animation
 }
 //event listener: 
 //clicked buttons of the game
@@ -161,17 +161,22 @@ boxes.forEach(box => {
 
 })
 //reset the game from scratch (difference with restart is the scores)
-btnReset.addEventListener("click",() =>{
+btnReset.addEventListener("click", () => {
     // reset turn to player 1
     player = 1
     //reset score of players
-    numScore1.textContent = "0"
-    numScore2.textContent = "0"
+    numScore1.textContent = ""
+    numScore2.textContent = ""
+    whoWinsParr.classList.remove("animate__zoomInDown")
+    h2.textContent = `The turn is for:`
+    h2.classList.remove("animate__tada")
+    document.querySelector(".p-one").value = "Name"
+    document.querySelector(".p-two").value = "Name"
     //reset name of players
-    document.querySelector(".playerone").textContent = "Player one" 
+    document.querySelector(".playerone").textContent = "Player one"
     document.querySelector(".playertwo").textContent = "Player Two"
-    counter = 0 
-    for (let i =0;i<boxes.length;i++) {
+    counter = 0
+    for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.backgroundColor = "#ffbb00cb"
         boxes[i].disabled = false
         boxes[i].textContent = ""
@@ -180,7 +185,7 @@ btnReset.addEventListener("click",() =>{
 //start - set up the values to start 
 start.addEventListener("click", () => {
     //change the name of player with the inputs names
-    name1 = document.querySelector(".p-one").value 
+    name1 = document.querySelector(".p-one").value
     name2 = document.querySelector(".p-two").value
     //display the names
     playername1.textContent = name1
@@ -192,8 +197,9 @@ start.addEventListener("click", () => {
     player = 1
     //display turn of player 1
     h2.textContent = `The turn is for:${name1}`
-    counter = 0 
-    
+    counter = 0
+    h2.classList.add("animate__tada")
+    h2.classList.remove("animate__jello")
 })
 
 
